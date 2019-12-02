18df46b796739c5770a4341c4df5d866f099da13
Kenechukwu <kenechukwu.ajufo@gmail.com>
2019-12-02 13:48:46 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+    APPLICATION_ID: process.env.APPLICATION_ID || 'ee2071b5add6fbe5512cbbb18882aa8907423132e4133bd1031f570ef7d9d9d1',
+    SECRET: process.env.SECRET || 'b982b868079a3305a8448d9338f0aa4bfae574e5d6917c07e8842a6fdceee940',
+    CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+}
